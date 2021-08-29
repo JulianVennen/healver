42721cd99b8f909474c6db76fe38a68da587f892
joshua <joshuaslui0203@gmail.com>
2021-08-29 20:24:40 +0200

    Add ping command

---
+++ commands/utility/ping.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'ping',
+
+    aliases: ['latency', 'pong'],
+
+    description: 'Get the bot and websocket latency',
+
+
+    async execute(message, args, client) {
+        const pong = await message.reply('Loading data');
+        await pong.edit(`🏓Latency is ${pong.createdTimestamp - message.createdTimestamp}ms.\nAPI Latency is ${Math.round(client.ws.ping)}ms`);
+    },
+};
