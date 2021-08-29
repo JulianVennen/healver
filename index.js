f09fdf1fe335c1eda8394b5e4083fa9c0851ab0d
joshua <joshuaslui0203@gmail.com>
2021-08-29 20:24:16 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const config = require('./config');
+const fs = require('fs');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], allowedMentions: { parse: [] } });
+
+//const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+client.commands = new Discord.Collection();
+const prefix = config.prefix
+
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
+}
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
+client.on('ready', () => {
+    client.user.setPresence( { activity: { type: 'WATCHING', name: 'my prefix (;)' }, status: 'dnd' } )
+    console.log('I am ready!');
+});
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) return;
+    //if (!client.commands.has(commandName)) return;
+    if (command.args && !args.length) {
+        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
+    try {
+        command.execute(message, args, client);
+    } catch (error) {
+        console.error(error);
+        message.reply(`there was an error trying to execute that command!\n\`${error}\``);
+    }
+});
+client.login(config.token);
