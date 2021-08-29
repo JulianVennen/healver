1e5d09cbccc79f92a5b772a487dd23151f8d4e0e
joshua <joshuaslui0203@gmail.com>
2021-08-29 21:28:53 +0200

    Add avatar command

---
+++ commands/utility/avatar.js
@@ -0,0 +1,35 @@
+const {User, Snowflake, MessageEmbed} = require('discord.js');
+const util = require('../../util');
+
+module.exports = {
+    name: 'avatar',
+
+    aliases: ['av', 'pfp'],
+
+    description: 'Check a user\'s avatar',
+
+
+    async execute(message, args, client) {
+        /** @type {User|Snowflake} */
+        let user = args.length ? util.userMentionToId(args[0]) : message.author;
+        if (!(user instanceof User)) {
+            try {
+                user = await client.users.fetch(user);
+            }
+            catch (e) {
+                if (e.httpStatus === 404) {
+                    await message.reply(`:warning: | Failed to find user ${args}`);
+                    return;
+                }
+                throw e;
+            }
+        }
+        const avatarEmbed = new MessageEmbed()
+            .setTitle(`Avatar of ${util.escapeFormatting(user.tag)}`)
+            .setImage(user.displayAvatarURL({dynamic: true, size: 2048}))
+            .setFooter(`Command executed by ${util.escapeFormatting(message.author.tag)}`)
+            .setTimestamp();
+
+        await message.reply({ embeds: [avatarEmbed] } );
+    },
+};
