bc61db9c083cd4b3b22a8cc18e249802ed5db13c
joshua <joshuaslui0203@gmail.com>
2021-08-30 15:32:53 +0200

    Restrict command to application owner

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -10,11 +10,14 @@ module.exports = {
     usage: '<command>',
 
     async execute(message, args, client) {
-        if (!client.application.owner) {
-            await message.reply(`This command is restricted to it's developers only.`)
+        if (!client.application.owner)
+            await client.application.fetch()
+
+        if (client.application.owner.id !== message.author.id) {
+            return message.reply('This command is restricted to my developers.')
         }
 
-        const commandName = args[0].toLowerCase();
+            const commandName = args[0].toLowerCase();
         const command = message.client.commands.get(commandName)
             || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
